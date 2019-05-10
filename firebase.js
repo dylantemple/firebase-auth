7f80ca205527ef1d4dbfd7fe5cd32c40ee4b0072
dylantemple <49735072+dylantemple@users.noreply.github.com>
2019-05-09 18:26:46 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,11 @@
-//Paste the details given to you by firebase under this comment
-//eg
-// var firebaseConfig = {
-// ...config
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+  var firebaseConfig = {
+    apiKey: "AIzaSyAPBHoSR-GxBrx9Ica95XKdypa6g8OpRNU",
+    authDomain: "fb-auth-example-a11e4.firebaseapp.com",
+    databaseURL: "https://fb-auth-example-a11e4.firebaseio.com",
+    projectId: "fb-auth-example-a11e4",
+    storageBucket: "fb-auth-example-a11e4.appspot.com",
+    messagingSenderId: "1084338458363",
+    appId: "1:1084338458363:web:3cdb46f865df46b4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
